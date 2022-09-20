07b7e9339335a66877d018f386557f40aecd0256
dmitry <dimryb@bk.ru>
2022-09-21 00:32:54 +0700

    Enable dark theme

--- app/src/main/java/space/rybakov/compose/MainActivity.kt
+++ app/src/main/java/space/rybakov/compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package space.rybakov.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -67,7 +68,12 @@ fun MessageCard(msg: Message) {
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     ComposeTheme {
