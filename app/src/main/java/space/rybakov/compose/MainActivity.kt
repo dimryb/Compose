53f986d63a18871fd9b4643461e9297c5bf5f741
dmitry <dimryb@bk.ru>
2022-09-25 23:06:23 +0700

    Create a list of messages

--- app/src/main/java/space/rybakov/compose/MainActivity.kt
+++ app/src/main/java/space/rybakov/compose/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,7 +19,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import space.rybakov.compose.tutorial.SampleData
 import space.rybakov.compose.ui.theme.ComposeTheme
+import space.rybakov.compose.tutorial.Message
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +35,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -50,7 +52,7 @@ fun MessageCard(msg: Message) {
 
         Column {
             Text(
-                text = msg.author,
+                text = msg.title,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
@@ -74,6 +76,7 @@ fun MessageCard(msg: Message) {
     showBackground = true,
     name = "Dark Mode"
 )
+
 @Composable
 fun PreviewMessageCard() {
     ComposeTheme {
@@ -82,5 +85,22 @@ fun PreviewMessageCard() {
                 msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
             )
         }
+    }
+}
+
+@Composable
+fun Conversations(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversations(){
+    ComposeTheme  {
+        Conversations(SampleData.conversationSample)
     }
 }
---
+++ app/src/main/java/space/rybakov/compose/tutorial/Message.kt
@@ -0,0 +1,6 @@
+package space.rybakov.compose.tutorial
+
+data class Message(
+    val title : String,
+    val body : String
+)
